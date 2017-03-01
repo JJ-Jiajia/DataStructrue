0996e017345ae11e5865f1b4f459437c791e82db
jim <jiajiawithjiajia@163.com>
2017-03-01 22:13:55 +0800

    顺序存储结构及链式存储结构线性表，不完全上传

--- 2.Linear_Lists/Linked_List.c
+++ 2.Linear_Lists/Linked_List.c
@@ -3,10 +3,10 @@
 #include "../1.PublicDefine/publicDefine.c"
 typedef struct {
     ElemType data;//指针等于说是数组
-    LinkList* next;
+    LinkList next;
 }LNode,*LinkList;
 //初始化函数
-LinkList* initLinkList(void){
+LinkList initLinkList(void){
     LinkList  L=(LinkList)malloc(sizeof(LNode));
     L->next=NULL;
     return  L;
@@ -16,36 +16,36 @@ Status CreateList(LinkList l,int n){
 
 }
 //销毁函数
-Status destroyList(LinkList* list){
+Status destroyList(LinkList list){
     if(!list) return ERROR;
     free(list);
     return OK;
 }
 //清空线性表
-Status  clearList(LinkList* list){
+Status  clearList(LinkList list){
     if(!list) return ERROR;
     list->length=0;
     return OK;
 }
 //判断是否空顺序线性表
-Status listEmpty(LinkList* list){
+Status listEmpty(LinkList list){
     if(!list) return ERROR;
     if(list->length==0) return TRUE;
     else return  FALSE;
 }
 //返回长度
-int listLength(LinkList* list){
+int listLength(LinkList list){
     if(!list) return ERROR;
     return list->length;
 }
 //返回第i个位置元素,从0开始
-ElemType getELem(LinkList* list,int i){
+ElemType getELem(LinkList list,int i){
     if(!list) return ERROR;
     if(i<0 || i>=list->length) return ERROR;
     return list->elem[i];
 }
 //返回元素在顺序表中的位置 返回i+1
-int locateElem(LinkList* list,ElemType e){
+int locateElem(LinkList list,ElemType e){
     int i;
     if(!list || listEmpty(list)) return ERROR;
     for(i=0;i<list->length;i++){
@@ -54,7 +54,7 @@ int locateElem(LinkList* list,ElemType e){
     if(i=list->length) return INFEASIBLE;
 }
 //返回前置元素
-ElemType priorElem(LinkList* list,ElemType e,ElemType* pre_e){
+ElemType priorElem(LinkList list,ElemType e,ElemType* pre_e){
     if(locateElem(list,e)) {
         *pre_e=getELem(list,ocateElem(list,e));
         return OK;
